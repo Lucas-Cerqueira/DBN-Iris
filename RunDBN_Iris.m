709705aa8e4b6f42e49f87340eb54adb59236408
Junior Moura <natmourajr@gmail.com>
2015-12-23 17:19:41 -0200

    Fine tune in Parameters (how to do it?)

--- RunDBN_Iris.m
+++ RunDBN_Iris.m
@@ -5,7 +5,7 @@ clear all;
 %load mnist_uint8;
 load fisheriris;
 
-addpath (genpath('./functions/'));
+addpath(genpath('./functions/'));
 
 str_targets = species;
 % 2 - Normalization (target)
@@ -23,7 +23,7 @@ for i = 1:size(str_targets,1)
     end
 end
 
-n_folds = 2;
+n_folds = 10;
 
 CVO = cvpartition(species,'Kfold',n_folds);
 for ifolds = 1%:n_folds
@@ -43,56 +43,40 @@ for ifolds = 1%:n_folds
     %Min = -1 and Max = 1
     [~, norm_fact] = mapminmax(meas(itrn,:)');
     data_norm = mapminmax('apply', meas' ,norm_fact);
-end
+
     
 
-%train_x = double(train_x) / 255;
-%test_x  = double(test_x)  / 255;
+	%train_x = double(train_x) / 255;
+	%test_x  = double(test_x)  / 255;
 
-train_x = (data_norm(:,itrn) + 1)./2;
-test_x = (data_norm(:,itst) + 1)./2;
-
-%train_y = double(train_y);
-%test_y  = double(test_y);
-
-train_y = targets(itrn,:)';
-test_y = targets(itst,:)';
-
-% %%  ex1 train a 100 hidden unit RBM and visualize its weights
-% rand('state',0)
-% %dbn.sizes = [100];
-% dbn.sizes = [75];
-% opts.numepochs =   1;
-% %opts.batchsize = 100;
-% opts.batchsize = size (meas,1)/n_folds;
-% opts.momentum  =   0;
-% opts.alpha     =   1;
-% dbn = dbnsetup(dbn, train_x', opts);
-% dbn = dbntrain(dbn, train_x', opts);
-% figure; visualize(dbn.rbm{1}.W');   %  Visualize the RBM weights
+	train_x = (data_norm(:,itrn) + 1)./2;
+	test_x = (data_norm(:,itst) + 1)./2;
 
 
-%%  ex2 train a 100-100 hidden unit DBN and use its weights to initialize a NN
-rand('state',0)
-%train dbn
-dbn.sizes = [75];
-opts.numepochs =   100;
-opts.batchsize =    75;
-opts.momentum  =    0;
-opts.alpha     =    1;
-dbn = dbnsetup(dbn, train_x', opts);
-dbn = dbntrain(dbn, train_x', opts);
+	%train_y = double(train_y);
+	%test_y  = double(test_y);
 
-%unfold dbn to nn
-nn = dbnunfoldtonn(dbn, 3);
-nn.activation_function = 'sigm';
+	train_y = targets(itrn,:)';
+	test_y = targets(itst,:)';
 
-%train nn
-opts.numepochs =  100;
-opts.batchsize = 5;
-nn = nntrain(nn, train_x', train_y', opts);
-[er, bad] = nntest(nn, test_x', test_y');
+	%%  ex2 train a 100-100 hidden unit DBN and use its weights to initialize a NN
+	rand('state',0)
+	%train dbn
+	dbn.sizes = [75];
+	opts.numepochs =   200;
+	opts.batchsize =    15;
+	opts.momentum  =    0.9;
+	opts.alpha     =    0.1;
+	dbn = dbnsetup(dbn, train_x', opts);
+	dbn = dbntrain(dbn, train_x', opts);
 
-assert(er < 0.10, 'Too big error');
+	%unfold dbn to nn
+	nn = dbnunfoldtonn(dbn, 3);
+	nn.activation_function = 'sigm';
 
-fprintf ('Acerto: %f\n', (1-er)*100);
+	%train nn
+	opts.numepochs =  100;
+	opts.batchsize = 5;
+	nn = nntrain(nn, train_x', train_y', opts);
+	[a,b,c,d] = confusion(targets(itst,:)',nnsim(nn,data_norm(:,itst)')')
+end
